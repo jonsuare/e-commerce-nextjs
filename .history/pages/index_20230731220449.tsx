691fd430fd80cdaa6d03cd73ca239aa6f1ebeb9d
Jonny Suarez <jonnysuarezjim@gmail.com>
2023-07-31 22:07:30 -0500

    first commit

---
+++ .history/pages/index_20230731220449.tsx
@@ -0,0 +1,8 @@
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function Home() {
+  return <h1 className="text-3xl font-bold">e-commerce</h1>;
+}
